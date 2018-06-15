f996b432ae61daa223e884ce7f955164b7decbd1
tanvikchw <39824811+tanvikchw@users.noreply.github.com>
2018-06-15 12:50:32 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './App.css';
+import TodoList from "./components/TodoList";
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to your todo list</h1>
+        </header>
+          <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
